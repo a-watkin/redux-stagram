8a12ef62dac5333d66e25ecda113d134edb6b848
adam <atomicpenguines@gmail.com>
2018-11-23 17:32:26 +0000

    adding routes

--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -8,9 +8,23 @@ import Main from './components/Main';
 import Single from './components/Single';
 import PhotoGrid from './components/PhotoGrid';
 
+// import react router
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+
+const router = (
+  <Router history={browserHistory}>
+    <Route path="/" component={Main}>
+      <IndexRoute component={PhotoGrid} />
+      <Route path="/view/:postId" component={Single} />
+    </Route>
+  </Router>
+);
+
 render(
   <div>
-    <Main />
+    <Main>
+      <p>grrr</p>
+    </Main>
     <Single />
     <PhotoGrid />
   </div>,
